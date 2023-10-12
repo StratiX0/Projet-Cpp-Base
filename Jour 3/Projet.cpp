55667dccf78e3e89638a10aebb7b056d9f7a83d6
Jérôme Hussenet <jerome.hussenet@sfr.fr>
2023-10-12 20:49:14 +0200

    v0.4

--- Jour 3/Projet.cpp
+++ Jour 3/Projet.cpp
@@ -112,98 +112,156 @@ int createVaisseau(int difficulte) {
 
 
 
-// Fonction permettant de créer le vaisseau du joueur en fonction de la difficulté
-//int createEnnemie(int difficulte) {
-//    // Initialisation du générateur de nombres aléatoires
-//    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-//
-//    // Définir des noms d'ennemis prédéfinis
-//    std::vector<std::string> ennemieNom = { "Xenomorphe", "Predator", "Garry" };
-//    int index = std::rand() % ennemieNom.size();
-//    std::string value = ennemieNom[index]; // un nom de vaisseau aléatoire pris de la liste
-//
-//    int hp = 250;
-//
-//    Ennemie ennemie;
-//
-//
-//    /* Memo class ennemie
-//    int id;
-//    std::string nom;
-//    int vie;
-//    int attack1;
-//    int attack2;
-//    int armure;
-//    int fuite;
-//    int regen;*/
-//
-//
-//    switch (difficulte) {
-//    case 1:
-//
-//        switch (value) {
-//
-//            case "Xenomorphe":
-//                ennemie.id = 0;
-//                ennemie.nom = value;
-//                ennemie.vie = 400;
-//                ennemie.missiles = 15;
-//                ennemie.plasma = 45;
-//                ennemie.mc = 300;
-//                ennemie.shieldPower = 30;
-//                ennemie.fsdCldw = 2;
-//                ennemie.reparation = 15;
-//                break;
-//
-//        }
-//
-//
-//        
-//
-//    case 2:
-//        vaisseau.id = 0;
-//        vaisseau.nom = value;
-//        vaisseau.vie = hp;
-//        vaisseau.missiles = 10;
-//        vaisseau.plasma = 30;
-//        vaisseau.mc = 200;
-//        vaisseau.shieldPower = 20;
-//        vaisseau.fsdCldw = 3;
-//        vaisseau.reparation = 10;
-//        break;
-//
-//    case 3:
-//        vaisseau.id = 0;
-//        vaisseau.nom = value;
-//        vaisseau.vie = hp;
-//        vaisseau.missiles = 5;
-//        vaisseau.plasma = 20;
-//        vaisseau.mc = 150;
-//        vaisseau.shieldPower = 10;
-//        vaisseau.fsdCldw = 4;
-//        vaisseau.reparation = 5;
-//        break;
-//
-//    default:
-//        // Gérer la valeur de difficulté par défaut ici
-//        break;
-//    }
-//
-//    std::ofstream VaisseauFile("Vaisseau.txt", std::ios::trunc);
-//
-//    if (!VaisseauFile.is_open()) {
-//        std::cerr << "Erreur : Impossible d'ouvrir le fichier." << std::endl;
-//        return -1; // Retourner une valeur d'erreur
-//    }
-//    else {
-//        VaisseauFile << "ID: " << vaisseau.id << std::endl << "Nom: " << vaisseau.nom << std::endl << "Vie: " << vaisseau.vie << std::endl << "Missiles: " << vaisseau.missiles << std::endl << "Plasma: " << vaisseau.plasma << std::endl << "MC: " << vaisseau.mc << std::endl << "Shield-Power: " << vaisseau.shieldPower << std::endl << "FSDcldw: " << vaisseau.fsdCldw << std::endl << "Reparation: " << vaisseau.reparation << std::endl;
-//        VaisseauFile.close();
-//    }
-//
-//    std::cout << "ID: " << vaisseau.id << std::endl << "Nom: " << vaisseau.nom << std::endl << "Vie: " << vaisseau.vie << std::endl << "Missiles: " << vaisseau.missiles << std::endl << "Plasma: " << vaisseau.plasma << std::endl << "MC: " << vaisseau.mc << std::endl << "Shield-Power: " << vaisseau.shieldPower << std::endl << "FSDcldw: " << vaisseau.fsdCldw << std::endl << "Reparation: " << vaisseau.reparation << std::endl;
-//
-//    return 0; // Renvoyer 0 pour indiquer le succès
-//}
+
+// Fonction permettant de créer un ennemi en fonction de la difficulté
+int createEnnemie(int difficulte) {
+    // Initialisation du générateur de nombres aléatoires
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    // Définir des noms d'ennemis prédéfinis
+    std::vector<std::string> ennemieNoms = { "Xenomorphe", "Predator", "Garry" };
+    int index = std::rand() % ennemieNoms.size();
+    std::string value = ennemieNoms[index]; // Un nom de vaisseau aléatoire pris de la liste
+
+    Ennemie ennemie;
+    ennemie.id = index + 1; // Utiliser l'index (0, 1, 2) comme ID (1, 2, 3)
+    ennemie.nom = value;
+
+    switch (difficulte) {
+
+    case 1:
+
+        switch (index) {
+
+        case 0:
+
+            ennemie.vie = 400;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 1:
+
+            ennemie.vie = 300;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 2:
+
+            ennemie.vie = 600;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+        }
+
+        break; // Ajoutez cette ligne pour sortir du premier switch
+
+    case 2:
+
+        switch (index) {
+
+        case 0:
+
+            ennemie.vie = 500;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 1:
+
+            ennemie.vie = 400;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 2:
+
+            ennemie.vie = 700;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        }
+
+        break; // Ajoutez cette ligne pour sortir du deuxième switch
+
+    case 3:
+
+        switch (index) {
+
+        case 0:
+
+            ennemie.vie = 600;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 1:
+
+            ennemie.vie = 500;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        case 2:
+
+            ennemie.vie = 1000;
+            ennemie.attack1 = 15;
+            ennemie.attack2 = 45;
+            ennemie.armure = 300;
+            ennemie.fuite = 30;
+            ennemie.regen = 2;
+            break;
+
+        }
+
+        break; // Ajoutez cette ligne pour sortir du troisième switch
+
+    default:
+
+        break;
+    }
+
+    std::ofstream EnnemiFile("Ennemi.txt", std::ios::trunc);
+
+    if (!EnnemiFile.is_open()) {
+        std::cerr << "Erreur : Impossible d'ouvrir le fichier." << std::endl;
+        return -1; // Retourner une valeur d'erreur
+    }
+    else {
+        EnnemiFile << "ID: " << ennemie.id << std::endl << "Nom: " << ennemie.nom << std::endl << "Vie: " << ennemie.vie << std::endl << "Attaque1: " << ennemie.attack1 << std::endl << "Attaque2: " << ennemie.attack2 << std::endl << "Armure: " << ennemie.armure << std::endl << "Fuite: " << ennemie.fuite << std::endl << "Regeneration: " << ennemie.regen << std::endl;
+        EnnemiFile.close();
+    }
+
+    std::cout << "ID: " << ennemie.id << std::endl << "Nom: " << ennemie.nom << std::endl << "Vie: " << ennemie.vie << std::endl << "Attaque1: " << ennemie.attack1 << std::endl << "Attaque2: " << ennemie.attack2 << std::endl << "Armure: " << ennemie.armure << std::endl << "Fuite: " << ennemie.fuite << std::endl << "Regeneration: " << ennemie.regen << std::endl;
+
+    return 0; // Renvoyer 0 pour indiquer le succès
+}
 
 
 
@@ -220,6 +278,8 @@ int main() {
     std::cin >> difficulte;
 
     createVaisseau(difficulte);
+
+    createEnnemie(difficulte);
 
     return 0;
 }
